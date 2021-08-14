eccfdb4435c19fc0c73a572fcefd1aa9419d2e36
Jlobblet <jlobblet-github@gmx.co.uk>
2021-08-14 13:11:08 +0100

    Regex fixes

--- WiringLanguage/Parsers/Strings.fs
+++ WiringLanguage/Parsers/Strings.fs
@@ -3,6 +3,6 @@ module WiringLanguage.Parsers.Strings
 open FParsec
 open FParsec.Pipes
 
-let EscapedString() = regexL "\".*?(?<!\\)(\\\\)*?\"" "Escaped string" |>> (fun s -> s.[1..^1])
+let EscapedString() = regexL @""".*?(?<!\\)(\\\\)*?""" "Escaped string" |>> (fun s -> s.[1..^1])
 
-let EscapedStringSemicolon() = regexL "\".*?(?<!\\)(\\\\)*?\";" "Escaped string semicolon" |>> (fun s -> s.[1..^2])
+let EscapedStringSemicolon() = regexL @""".*?(?<!\\)(\\\\)*?"";" "Escaped string semicolon" |>> (fun s -> s.[1..^2])
