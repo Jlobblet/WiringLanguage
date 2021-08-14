47026388544278e635ce9cfad0139e1e2c0c2387
Jlobblet <jlobblet-github@gmx.co.uk>
2021-08-14 13:19:03 +0100

    Fix test sample

--- WiringLanguage.ItemAssembly/Program.fs
+++ WiringLanguage.ItemAssembly/Program.fs
@@ -6,10 +6,10 @@ open WiringLanguage.Interpreter
 [<EntryPoint>]
 let main argv =
     """
-    #import <%HOME%/Documents/Barotrauma/WiringLangugage/WiringLanguage.StandardLibrary/bin/Debug/net5.0/standard library.wl>;
+    #import <%HOME%/Documents/Barotrauma/WiringLanguage/WiringLanguage.StandardLibrary/bin/Debug/net5.0/standard library.wl>;
     Memory m1, m2, m_out;
-    m1.Value = 600;
-    m2.Value = 21;
+    m1.Value = "600";
+    m2.Value = "21";
     Adder a;
     m1.signal_out -> a.signal_in1;
     m2.signal_out -> a.signal_in2;
